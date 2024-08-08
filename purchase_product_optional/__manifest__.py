d668182c288a5e6a6b12a57b5cf455d75990f789
khoerulmutaqin225 <khoerulmutaqin225@gmail.com>
2024-08-08 11:25:01 +0700

    add  purchase_product_optional 16.0

---
+++ purchase_product_optional/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Product Optional",
+    'version': '1.0',
+    'category': 'VDL',
+    'summary': "Optional products feature",
+    "author": "Doodex",
+    "application": True,
+
+    'description': """
+Technical module:
+The main purpose is to enables the "optional products" feature on purchase order.
+    """,
+
+    'depends': ['purchase', 'purchase_product_matrix'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'demo': [],
+    'assets': {
+        'web.assets_backend': [
+            'purchase_product_optional/static/src/js/variant_mixin.js',
+            'purchase_product_optional/static/src/js/purchase_product_field.js',
+            'purchase_product_optional/static/src/js/product_configurator_modal.js',
+        ],
+    },
+    'auto_install': True,
+    'license': 'AGPL-3',
+}
---
+++ purchase_product_optional/controllers/main.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+from odoo.http import request
+
+
+class ProductConfiguratorPurchaseController(http.Controller):
+    @http.route(['/purchase_product_optional/configure'], type='json', auth="user", methods=['POST'])
+    def configure(self, product_template_id, pricelist_id, **kw):
+        add_qty = float(kw.get('add_qty', 1))
+        product_template = request.env['product.template'].browse(int(product_template_id))
+        pricelist = self._get_pricelist(pricelist_id)
+
+        product_combination = False
+        attribute_value_ids = set(kw.get('product_template_attribute_value_ids', []))
+        attribute_value_ids |= set(kw.get('product_no_variant_attribute_value_ids', []))
+        if attribute_value_ids:
+            product_combination = request.env['product.template.attribute.value'].browse(
+                attribute_value_ids
+            ).filtered(
+                lambda ptav: ptav.product_tmpl_id == product_template
+            )  # Filter out ptavs not belonging to the given template
+            # It happens when you change the template on an already configured line
+            # receiving the configured attributes data from the previous template configuration.
+
+        if pricelist:
+            product_template = product_template.with_context(pricelist=pricelist.id, partner=request.env.user.partner_id)
+
+        return request.env['ir.ui.view']._render_template(
+            "purchase_product_optional.configure",
+            {
+                'product': product_template,
+                'pricelist': pricelist,
+                'add_qty': add_qty,
+                'product_combination': product_combination
+            },
+        )
+
+    @http.route(['/purchase_product_optional/show_advanced_configurator'], type='json', auth="user", methods=['POST'])
+    def show_advanced_configurator(self, product_id, variant_values, pricelist_id, **kw):
+        pricelist = self._get_pricelist(pricelist_id)
+        return self._show_advanced_configurator(product_id, variant_values, pricelist, False, **kw)
+
+    @http.route(['/purchase_product_optional/optional_product_items'], type='json', auth="user", methods=['POST'])
+    def optional_product_items(self, product_id, pricelist_id, **kw):
+        pricelist = self._get_pricelist(pricelist_id)
+        return self._optional_product_items(product_id, pricelist, **kw)
+
+    def _optional_product_items(self, product_id, pricelist, **kw):
+        add_qty = float(kw.get('add_qty', 1))
+        product = request.env['product.product'].browse(int(product_id))
+
+        parent_combination = product.product_template_attribute_value_ids
+        if product.env.context.get('no_variant_attribute_values'):
+            # Add "no_variant" attribute values' exclusions
+            # They are kept in the context since they are not linked to this product variant
+            parent_combination |= product.env.context.get('no_variant_attribute_values')
+
+        return request.env['ir.ui.view']._render_template("purchase_product_optional.optional_product_items", {
+            'product': product,
+            'parent_name': product.name,
+            'parent_combination': parent_combination,
+            'pricelist': pricelist,
+            'add_qty': add_qty,
+        })
+
+    def _show_advanced_configurator(self, product_id, variant_values, pricelist, handle_stock, **kw):
+        product = request.env['product.product'].browse(int(product_id))
+        combination = request.env['product.template.attribute.value'].browse(variant_values)
+        add_qty = float(kw.get('add_qty', 1))
+
+        no_variant_attribute_values = combination.filtered(
+            lambda product_template_attribute_value: product_template_attribute_value.attribute_id.create_variant == 'no_variant'
+        )
+        if no_variant_attribute_values:
+            product = product.with_context(no_variant_attribute_values=no_variant_attribute_values)
+
+        return request.env['ir.ui.view']._render_template("purchase_product_optional.optional_products_modal_purchase", {
+            'product': product,
+            'combination': combination,
+            'add_qty': add_qty,
+            'parent_name': product.name,
+            'variant_values': variant_values,
+            'pricelist': pricelist,
+            'handle_stock': handle_stock,
+            'already_configured': kw.get("already_configured", False),
+            'mode': kw.get('mode', 'add'),
+            'product_custom_attribute_values': kw.get('product_custom_attribute_values', None),
+            'no_attribute': kw.get('no_attribute', False),
+            'custom_attribute': kw.get('custom_attribute', False)
+        })
+
+    def _get_pricelist(self, pricelist_id, pricelist_fallback=False):
+        return request.env['product.pricelist'].browse(int(pricelist_id or 0))
+
+    @http.route(['/purchase_product_optional/get_combination_info'], type='json', auth="user", methods=['POST'])
+    def get_combination_info(self, product_template_id, product_id, combination, add_qty, pricelist_id, **kw):
+        combination = request.env['product.template.attribute.value'].browse(combination)
+        pricelist = self._get_pricelist(pricelist_id)
+        cids = request.httprequest.cookies.get('cids', str(request.env.user.company_id.id))
+        allowed_company_ids = [int(cid) for cid in cids.split(',')]
+        ProductTemplate = request.env['product.template'].with_context(allowed_company_ids=allowed_company_ids)
+        if 'context' in kw:
+            ProductTemplate = ProductTemplate.with_context(**kw.get('context'))
+        product_template = ProductTemplate.browse(int(product_template_id))
+        res = product_template._get_combination_info_purchase(combination, int(product_id or 0), int(add_qty or 1), pricelist)
+        if 'parent_combination' in kw:
+            parent_combination = request.env['product.template.attribute.value'].browse(kw.get('parent_combination'))
+            if not combination.exists() and product_id:
+                product = request.env['product.product'].browse(int(product_id))
+                if product.exists():
+                    combination = product.product_template_attribute_value_ids
+            res.update({
+                'is_combination_possible': product_template._is_combination_possible(combination=combination,
+                                                                                     parent_combination=parent_combination),
+                'parent_exclusions': product_template._get_parent_attribute_exclusions(
+                    parent_combination=parent_combination)
+            })
+        return res
+
+    @http.route(['/purchase_product_optional/create_product_variant'], type='json', auth="user", methods=['POST'])
+    def create_product_variant(self, product_template_id, product_template_attribute_value_ids, **kwargs):
+        return request.env['product.template'].browse(int(product_template_id)).create_product_variant(
+            json.loads(product_template_attribute_value_ids))
